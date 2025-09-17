bb0858fed6ccb8fb7ae7ff75cb73ef8b03825bbf
Valentin-Igrevsky <valentin.igrevsky@gmail.com>
2025-09-17 17:59:28 +0300

    UPD L1 ADD img

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Валя"); // Вывод строки "Валя" в консоль
+        System.out.println("Валентин"); // Вывод строки "Валя" в консоль
         System.out.println(456); // Вывод числа 456 в консоль
 
         // Переменные и примитивные типы данных
@@ -11,7 +11,7 @@ public class Main {
         boolean abc_boolean_t = true; // Небо синее - правда
         boolean abc_boolean_f = false; // Трава красная - ложь
         char abc_char = 432; // Один символ
-        String abc1 = "Valya"; // Текст (String с английского "Строка"; Не примитивный тип, но очень важный)
+        String abc_string = "Valentin"; // Текст (String с английского "Строка"; Не примитивный тип, но очень важный)
 
         // Математические операции
         System.out.println(2 + 2); // Сложение
