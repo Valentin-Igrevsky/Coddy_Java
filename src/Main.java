8b7ff5eddc121e45b4ba33582ebaa011fedaadf0
Valentin-Igrevsky <valentin.igrevsky@gmail.com>
2025-09-17 13:27:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Валя"); // Вывод строки "Валя" в консоль
+        System.out.println(456); // Вывод числа 456 в консоль
+
+        // Переменные и примитивные типы данных
+        int abc = 456; // Целые числа
+        float abc_float = 43.54f; // Дробные числа
+        double abc_double = 43.54; // Большие* дробные числа
+        boolean abc_boolean_t = true; // Небо синее - правда
+        boolean abc_boolean_f = false; // Трава красная - ложь
+        char abc_char = 432; // Один символ
+        String abc1 = "Valya"; // Текст (String с английского "Строка"; Не примитивный тип, но очень важный)
+
+        // Математические операции
+        System.out.println(2 + 2); // Сложение
+        System.out.println(100 - 50); // Вычетание
+        System.out.println(54356 * 8546); // Умножение
+        System.out.println(4 / 2); // Деление
+
+        System.out.println(abc_char);
+
+    }
+}
